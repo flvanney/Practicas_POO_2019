c55ac0a9562c1299ee52994b35abf48e758783ed
FrancoVanney <49128594+flvanney@users.noreply.github.com>
2019-08-31 01:24:52 -0300

    Delete Estado.java

--- Estado.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.ArrayList;
-
-public interface Estado {
-    
-    public abstract void agregarArticulos(float precio, int cantidad, ArrayList<Articulo> articulos);
-    public abstract void agregarRemito(String descripcion, ArrayList<Remito> remito);
-}
